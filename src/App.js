549ed94a6771a772e4a8eafa34143f35d7bcdc49
윤성 <yunyul1209@gmail.com>
2022-05-08 18:49:39 +0900

    feat(*) : learn basic settings and layouts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyHeader from "./MyHeader";
+import MyFooter from "./MyFooter";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello React!</h2>
+        <Counter />
       </header>
+      <MyFooter />
     </div>
   );
 }
