cbeee586bd0d3e9003ca578c2c5e2762be57feb5
윤성 <yunyul1209@gmail.com>
2022-05-09 16:38:22 +0900

    feat(*) : basic grammar related code modification

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import "./App.css";
 import MyHeader from "./MyHeader";
 import MyFooter from "./MyFooter";
 import Counter from "./Counter";
+import Container from "./Container";
 
 function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    initialValue: 5,
+  };
+
   return (
     <div className="App">
       <MyHeader />
       <header className="App-header">
         <h2>Hello React!</h2>
-        <Counter />
+        <Counter {...counterProps} />
       </header>
       <MyFooter />
     </div>
--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,23 @@
-const Counter = () => {
+import React, { useState } from "react";
+import OddEventResult from "./OddEvenResult";
+
+const Counter = ({ initialValue }) => {
+  const [count, setCount] = useState(initialValue);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
-      <h2>0</h2>
-      <button>+</button>
-      <button>-</button>
+      <h2>{count}</h2>
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+      <OddEventResult count={count} />
     </div>
   );
 };
