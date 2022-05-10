daf956351727dc16a2e5b163bd4d38127a60f9e8
윤성 <yunyul1209@gmail.com>
2022-05-10 16:12:48 +0900

    feat(useRef) : Manipulating DOM using useRef
    React에서 DOM 조작하기 - useRef

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,31 +1,74 @@
 /* eslint-disable */
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
-  // 작성자 input 관리하기 위한 state 생성
-  const [author, setAuthor] = useState("");
-  // 본문 내용 textarea 관리하기 위한 state 생성
-  const [content, setContent] = useState("");
+  // DOM 객체에 접근하기 위한 useRef 레퍼런스 객체 생성
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  // 작성자 input & 본문내용 textarea 관리하기 위한 state 생성
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      // focus
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      // focus
+      contentInput.current.focus();
+      return;
+    }
+    console.log(state);
+    alert("저장 성공!");
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
-          value={author}
-          onChange={e => {
-            setAuthor(e.target.value);
-          }}
+          value={state.author}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
-          value={content}
-          onChange={e => {
-            setContent(e.target.value);
-          }}
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
         />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
