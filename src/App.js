6fa43bdbd3627408880daaa8730ab86d88dc7d94
윤성 <yunyul1209@gmail.com>
2022-05-11 13:45:31 +0900

    feat(*) : Add function to render diary list
    다이어리 리스트 조회를 위한 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "멋쟁이토마토",
+    content: "일기 리스트 내용 1",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "알감자",
+    content: "일기 리스트 내용 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "윤성",
+    content: "일기 리스트 내용 3",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <DiaryEditor />
-      </header>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
