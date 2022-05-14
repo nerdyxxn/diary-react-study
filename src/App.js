f7ef5a88702708f3a44de06b84a3a5d977d42674
윤성 <yunyul1209@gmail.com>
2022-05-14 22:43:16 +0900

    feat(*) : Add API data inquiry function using axios
    axios를 이용한 API 데이터 호출하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   // 1. 일기 데이터를 관리할 state 생성
@@ -10,6 +13,38 @@ function App() {
   // 2. onCreate 함수 생성 -> 데이터 추가 기능 구현
   // id는 useRef를 활용해 현재값 current + 1 형태로 구현
   const dataId = useRef(0);
+
+  // const getData = async () => {
+  //   const res = await fetch(
+  //     "https://jsonplaceholder.typicode.com/comments"
+  //   ).then(res => res.json());
+  //   console.log(res);
+  // };
+  const getData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then(res => {
+        //결과값 : res.data
+        const initData = res.data.slice(0, 20).map(it => {
+          return {
+            author: it.email,
+            content: it.body,
+            emotion: Math.floor(Math.random() * 5) + 1,
+            created_date: new Date().getTime(),
+            id: dataId.current++,
+          };
+        });
+        setData(initData);
+      })
+      .catch(() => {
+        console.log("데이터 호출 실패!");
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
